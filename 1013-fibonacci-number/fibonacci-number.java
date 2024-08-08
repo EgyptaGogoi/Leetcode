d85b8ffd4c1e48fb97eb02f4d12e2c240a0b2eaf
Egypta Gogoi <egypta31@gmail.com>
2024-08-09 00:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (10.04%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,19 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)
-            return 0;
+        // if(n==0)
+        //     return 0;
+        // if(n==1)
+        //     return 1;
+        // return fib(n-1)+fib(n-2);
         if(n==1)
             return 1;
-        return fib(n-1)+fib(n-2);
+        int sum=0,a=0,b=1;
+        while(n-1>0){
+            sum=a+b;
+            a=b;
+            b=sum;
+            n--;
+        }
+        return sum;
     }
 }
