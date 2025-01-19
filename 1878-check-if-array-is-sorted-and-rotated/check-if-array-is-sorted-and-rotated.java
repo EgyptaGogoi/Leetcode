ab3b36fa95a4c39d7dc07a74f82bb507f60b1e1f
Egypta Gogoi <egyptagogoi@gmail.com>
2025-01-19 22:52:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (36.64%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean check(int[] nums) {
-        // if(nums.length==2 || nums.length==1)
-        //     return true;
         int curr=nums[0],drop=0;
         for(int i=1; i<nums.length; i++){
             if(curr>nums[i])
                 drop++;
             curr=nums[i];
         }
-        System.out.print(drop);
         if(drop==1 && nums[0]<nums[nums.length-1])
             return false;
         if(drop>1)
             return false;
         return true;
-        // if(drop)
     }
 }
