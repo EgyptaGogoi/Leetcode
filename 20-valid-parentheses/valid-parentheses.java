5e0a8e496e8cb9d8405aa1c3c2eb75526599e923
Egypta Gogoi <egyptagogoi@gmail.com>
2025-04-23 12:01:07 +0530

    Time: 3 ms (41.79%) | Memory: 41.8 MB (66.36%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,22 +2,19 @@ class Solution {
     public boolean isValid(String s) {
         if(s.length()%2!=0)
             return false;
-        char[] par = s.toCharArray();
         Stack<Character> stack = new Stack<>();
         int i;
-        for (i=0; i<par.length ; i++){
-            if(par[i]=='{' || par[i]=='[' || par[i]=='(')
-                stack.push(par[i]);   
+        for (i=0; i<s.length() ; i++){
+            if(s.charAt(i)=='{' || s.charAt(i)=='[' || s.charAt(i)=='(')
+                stack.push(s.charAt(i));   
             else if(!stack.isEmpty() && 
-            (par[i]=='}'&& stack.peek()=='{' || 
-                    par[i]==']'&& stack.peek()=='[' ||
-                    par[i]==')'&& stack.peek()=='(')){
-                        stack.pop();
-            }   
+            (s.charAt(i)=='}'&& stack.peek()=='{' || 
+                    s.charAt(i)==']'&& stack.peek()=='[' ||
+                    s.charAt(i)==')'&& stack.peek()=='('))
+                        stack.pop();  
             else
                 return false; 
         }
-        System.out.print(stack);
         return stack.isEmpty();
                 
     }
