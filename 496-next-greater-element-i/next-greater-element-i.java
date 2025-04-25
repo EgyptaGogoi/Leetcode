19866556def1e731f5c5a6a3f61c09c9a9a979e6
Egypta Gogoi <egyptagogoi@gmail.com>
2025-04-25 17:11:22 +0530

    Time: 4 ms (36.74%) | Memory: 42.9 MB (99.06%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int ans[] = new int[nums1.length];
-        int i,j,curr=0,flag=0;
+        int i,j,flag=0;
         for(i=0; i<nums1.length; i++){
             ans[i]=-1;
             j=0;
             flag=0;
-            // curr=-1;
             while(j<nums2.length){
                 if(nums1[i]==nums2[j]){
-                    // curr=nums2[j];
                     flag=1;
                 } 
                 if(flag==1 && nums1[i]<nums2[j]){
@@ -19,7 +17,6 @@ class Solution {
                 j++;     
             }
         }
-        // System.out.print(flag);
         return ans;
     }
 }
