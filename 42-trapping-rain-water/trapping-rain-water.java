b8ddaec1cef5268ba2b51af25bbefd9827b92203
Egypta Gogoi <egyptagogoi@gmail.com>
2025-05-04 02:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (50.30%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,33 +1,31 @@
 class Solution {
     public int trap(int[] height) {
-        int []LG = new int[height.length];
-        int []RG = new int[height.length];
-        int sum=0,min,i,currg,n=height.length;
-        currg=0;
-        for(i=0; i<n; i++){
-            if(currg>height[i])
-                LG[i]=currg;
-            else{
-                LG[i]=currg;
-                currg=height[i];
+        int RG, LG, currR, currL, sum = 0, n = height.length, min;
+        RG = height[n - 1];
+        LG = height[0];
+        currL = 0;
+        currR = n - 1;
+
+        while (currL < currR) {
+            if (height[currL] < height[currR]) {
+                currL++;
+                if (height[currL] >= LG) {
+                    LG = height[currL]; // update LG
+                } else {
+                    min = Math.min(LG, RG); // LG already up to date
+                    sum += min - height[currL];
+                }
+            } else {
+                currR--;
+                if (height[currR] >= RG) {
+                    RG = height[currR]; // update RG
+                } else {
+                    min = Math.min(LG, RG); // RG already up to date
+                    sum += min - height[currR];
+                }
             }
-            // System.out.println(LG[i]);
         }
-        currg=0;
-        for(i=n-1; i>=0; i--){
-            if(currg>height[i])
-                RG[i]=currg;
-            else{
-                RG[i]=currg;
-                currg=height[i];
-            }
-            // System.out.println(RG[i]);
-        }
-        for(i=0; i<n; i++){
-            min=Math.min(LG[i],RG[i]);
-            if(min-height[i]>=0)
-                sum+=min-height[i];
-        }
+
         return sum;
     }
 }
