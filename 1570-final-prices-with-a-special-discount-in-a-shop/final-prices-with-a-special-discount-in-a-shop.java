6f53d1455b8b70eaae94def8dd70d75a85b662b1
Egypta Gogoi <egyptagogoi@gmail.com>
2025-06-11 12:00:13 +0530

    Time: 3 ms (52.02%) | Memory: 43.7 MB (59.33%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,23 +1,8 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int []ans = new int[prices.length];
-        int i, j, minj ,flag,disc;
+        int i, j,disc;
         for(i=0; i<prices.length-1; i++){
-            // j=prices.length-1;
-            // minj=prices.length-1;
-            // flag=0;
-            // while(j>i){
-            //     if(minj>j && prices[j]<=prices[i]){
-            //         minj=j;
-            //         flag=1;
-            //     } 
-            //     j--;
-            // }
-            // System.out.print(prices[minj]);
-            // if(flag<1)
-            //     ans[i]=prices[i];
-            // else
-            //     ans[i]=prices[i]-prices[minj];
             if(prices[i+1]<prices[i]){
                 ans[i]=prices[i]-prices[i+1];
             }
