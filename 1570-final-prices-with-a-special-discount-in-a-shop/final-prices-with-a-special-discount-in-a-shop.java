90ef973f68c36681ef3bddeff6cdd965ebcb3bb4
Egypta Gogoi <egyptagogoi@gmail.com>
2025-06-11 12:04:44 +0530

    Time: 4 ms (33.88%) | Memory: 43.6 MB (74.15%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -2,11 +2,11 @@ class Solution {
     public int[] finalPrices(int[] prices) {
         int []ans = new int[prices.length];
         int i, j,disc;
-        for(i=0; i<prices.length-1; i++){
-            if(prices[i+1]<prices[i]){
-                ans[i]=prices[i]-prices[i+1];
-            }
-            else{
+        for(i=0; i<prices.length; i++){
+            // if(prices[i+1]<prices[i]){
+            //     ans[i]=prices[i]-prices[i+1];
+            // }
+            // else{
                 j=prices.length-1;
                 disc=0;
                 while(j>i){
@@ -16,9 +16,9 @@ class Solution {
                     j--;
                 }
                 ans[i]=prices[i]-disc;  
-            }
+            // }
         }
-        ans[i]=prices[i];
+        // ans[i]=prices[i];
         return ans;
     }
 }
