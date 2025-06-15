d96493f1438c4fbc274599b8ed04fcebc46d95ed
Egypta Gogoi <egyptagogoi@gmail.com>
2025-06-16 00:56:03 +0530

    Time: 44 ms (29.41%) | Memory: 13.5 MB (85.29%) - LeetSync

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def validStrings(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        queue= deque()
+        ls=[]
+        i=1
+        queue.append("0")
+        queue.append("1")
+        extras=["0","1"]
+        while queue:
+            s=queue.popleft()
+            if len(s) < n :
+                for e in extras:
+                    ss=s+e
+                    if ss[len(ss)-1] == "0" and ss[len(ss)-2] == "0":
+                        continue 
+                    queue.append(ss)
+            else :
+                ls.append(s)
+        return ls
+            
+            
+    
