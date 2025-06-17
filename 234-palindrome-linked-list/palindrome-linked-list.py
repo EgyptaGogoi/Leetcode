298a018692ae3e31822772049bb96448736f0922
Egypta Gogoi <egyptagogoi@gmail.com>
2025-06-17 16:11:20 +0530

    Time: 997 ms (5.00%) | Memory: 83.7 MB (34.06%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -7,18 +7,13 @@ class Solution(object):
     def reversedList(self, head):
         prev = None
         curr = head
-        # suppose curr is at 2
+
         while curr:
-            # next node to go (1->2->3 so 3)
             nexnode = curr.next 
-            # change the next to point to the previous node (e.g. 2->1->X)
             curr.next = prev
-            # next time the prev should be curr
             prev = curr
-            # move curr to next node (4)
             curr = nexnode
-        # we dont return curr as curr is 
-        # at None so previous of curr is prev
+            
         return prev 
 
     def isPalindrome(self, head):
@@ -26,25 +21,21 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: bool
         """
-        oldlist= ListNode(head.val)
-        oldcurr= head.next
-        newcurr=oldlist
+        oldlist = ListNode(head.val)
+        oldcurr = head.next
+        newcurr = oldlist
+
         while oldcurr :
-            # if revlist.val != head.val :
-            #     return false
             newcurr.next= ListNode(oldcurr.val)
             oldcurr=oldcurr.next
             newcurr=newcurr.next
-        # while oldlist:
-        #     print(str(oldlist.val)+"->") 
-        #     oldlist=oldlist.next
-        # print("\n")
-        # curr=head
+        
         revlist = self.reversedList(head)
+
         while revlist :
             if revlist.val != oldlist.val :
                 return False
-        #     print(str(dummy.val)+"->")
             revlist = revlist.next
             oldlist = oldlist.next
+
         return True       
