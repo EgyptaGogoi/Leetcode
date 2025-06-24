26711b3cf1b89da25f59b7729ab38c9d263fca21
Egypta Gogoi <egyptagogoi@gmail.com>
2025-06-24 23:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (80.53%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        j = len(nums)-1
+        while i<j:
+            if nums[i] != val:
+                i+=1
+            elif nums[i] == val and nums[j] != val:
+                temp = nums[i]
+                nums[i]=nums[j]
+                nums[j]= temp
+                i+=1
+                j-=1
+            else:
+                j-=1
+
+        count=0
+        for ele in nums:
+            if ele != val:
+                count+=1
+        return count
