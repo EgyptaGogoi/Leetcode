3f2aaa13c7826c4f24d30d0d26585379dd56a519
Egypta Gogoi <egyptagogoi@gmail.com>
2025-07-03 16:24:02 +0530

    Time: 21 ms (48.00%) | Memory: 12.4 MB (82.00%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
@@ -5,12 +5,10 @@ class Solution(object):
         :rtype: str
         """
         word = "a"
-       
         while len(word) <= k:
             neword = ""
             for ch in word :
                 asc = 97 if ch == 'z' else ord(ch)+1
                 neword += chr(asc)
             word += neword
-            print(word)
         return word[k-1]
