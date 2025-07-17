3334eabb1e3b1a5156d6615240f4ed19a86f6ab9
Egypta Gogoi <egyptagogoi@gmail.com>
2025-07-17 23:44:28 +0530

    Time: 7 ms (69.69%) | Memory: 12.5 MB (52.63%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -5,30 +5,21 @@ class Solution(object):
         :rtype: int
         """
         roman = {
-            "I":1,
-            "V":5,
-            "X":10,
-            "L":50,
-            "C":100,
-            "D":500,
-            "M":1000
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
         sum = 0
         i = 0
-        while i< len(s):
-            if s[i] == 'I'and i+1 <len(s) and s[i+1] in {'V', 'X'}:
-                    sum += roman[s[i+1]] - roman[s[i]]
-                    i+=2
-            elif s[i] == 'X' and i+1<len(s) and s[i+1] in {'L', 'C'}:
-                    sum += roman[s[i+1]] - roman[s[i]]
-                    i+=2
-            elif s[i] == 'C'and i+1<len(s) and s[i+1] in {'D', 'M'}:
-                    sum += roman[s[i+1]] - roman[s[i]]
-                    i+=2
+        while i < len(s):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                sum += roman[s[i + 1]] - roman[s[i]]
+                i += 2  
             else:
-                sum+= roman[s[i]]
-                i+=1
-
+                sum += roman[s[i]]
+                i += 1
         return sum
-
-          
