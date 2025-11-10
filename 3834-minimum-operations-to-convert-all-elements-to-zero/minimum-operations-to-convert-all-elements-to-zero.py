114f24fd1303507b4d275024e0b39f94eeea1bcb
Egypta Gogoi <egyptagogoi@gmail.com>
2025-11-11 00:28:15 +0530

    Time: 362 ms (28.57%) | Memory: 21.3 MB (42.86%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.py
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.py
@@ -10,21 +10,9 @@ class Solution(object):
         for ele in nums:
             if ele == 0:
                 flag = 1
-            # if not st:
-            #     st.append(ele)
-            #     ops+=1
-            # elif st[-1] = ele:
-            #     continue
-            # elif st[-1] > ele:
             while st and st[-1] > ele  :
                 st.pop()
             if not st or st[-1]!=ele :
                 st.append(ele)
-                ops+=1
-            # elif 
-            #     st.append(ele)
-            #     ops+=1
-            # else:
-            #     st.append(ele)
-            #     ops+=1  
+                ops+=1    
         return ops-flag
