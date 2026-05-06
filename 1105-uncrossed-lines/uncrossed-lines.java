93c3db25f3d83268668bb67bd9b32cd415fbc997
Egypta Gogoi <egyptagogoi@gmail.com>
2026-05-07 02:50:37 +0530

    Time: 6 ms (80.46%) | Memory: 46.1 MB (27.10%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -2,22 +2,27 @@
 class Solution {
     private int memo[][];
 
-    private void initMemoForBC(int[][] memo, int m, int n){
-    // initialize memo for Base Condition
-        for (int i = 0; i<=m; i++){
-            for (int j = 0; j<=n; j++){
-                if (i==0 || j == 0) // if length of x, y == 0
-                    memo[i][j] = 0; // No matching char
-            }
-        }
-    }
+    // private void initMemoForBC(int[][] memo, int m, int n){
+    // // initialize memo for Base Condition
+    //     for (int i = 0; i<=m; i++){
+    //         for (int j = 0; j<=n; j++){
+                
+    //         }
+    //     }
+    // }
 
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
         this.memo = new int[m+1][n+1]; // need t[m][n]
-        initMemoForBC(memo, m, n);
+        // initMemoForBC(memo, m, n);
         int i, j;
+        for (i = 0; i<=m; i++){
+            for (j = 0; j<=n; j++){
+                if (i==0 || j == 0) // if length of x, y == 0
+                    memo[i][j] = 0; // No matching char
+            }
+        }
         for (i = 1; i<=m; i++){ // starting with 1, did the BC with 0
             for (j = 1; j<=n; j++){
                 if (nums1[i-1] == nums2[j-1]) //x[i-1] == y[j=1]
