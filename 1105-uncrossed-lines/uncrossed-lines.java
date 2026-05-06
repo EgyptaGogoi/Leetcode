544009f9ed2c3950476ef0a425bc12d4b0e3778d
Egypta Gogoi <egyptagogoi@gmail.com>
2026-05-07 02:47:17 +0530

    Time: 8 ms (11.27%) | Memory: 46 MB (52.35%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -1,31 +1,31 @@
-// recursion + memoization (table)
+// Memoization + DP top-down 
 class Solution {
     private int memo[][];
-    private void initMemo(int[][] memo, int m, int n){
+
+    private void initMemoForBC(int[][] memo, int m, int n){
+    // initialize memo for Base Condition
         for (int i = 0; i<=m; i++){
             for (int j = 0; j<=n; j++){
-                memo[i][j] = -1;
+                if (i==0 || j == 0) // if length of x, y == 0
+                    memo[i][j] = 0; // No matching char
             }
         }
     }
-    private int LCS(int[] x, int[] y, int m, int n) {
-        // BC :
-        if (m == 0 || n == 0)
-            return 0;
-        if (memo[m][n] != -1)
-            return memo[m][n];
-        if (x[m-1] == y[n-1])
-            memo[m][n] = 1 + LCS(x,y,m-1,n-1);
-        else
-            memo[m][n] = Math.max(LCS(x,y,m,n-1), LCS(x,y,m-1,n));
-        return memo[m][n];
-    }
+
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
         this.memo = new int[m+1][n+1]; // need t[m][n]
-        initMemo(memo, m, n);
-        return LCS(nums1, nums2, m, n);
-
+        initMemoForBC(memo, m, n);
+        int i, j;
+        for (i = 1; i<=m; i++){ // starting with 1, did the BC with 0
+            for (j = 1; j<=n; j++){
+                if (nums1[i-1] == nums2[j-1]) //x[i-1] == y[j=1]
+                    memo[i][j] = 1 + memo[i-1][j-1];
+                else
+                    memo[i][j] = Math.max(memo[i][j-1], memo[i-1][j]);
+            }
+        }
+        return memo[m][n];
     }
 }
