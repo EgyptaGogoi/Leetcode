7eac15202ad487acf5e3177416138a20201b3983
Egypta Gogoi <egyptagogoi@gmail.com>
2026-05-11 22:50:39 +0530

    Time: 1 ms (99.92%) | Memory: 94.8 MB (14.87%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
         int max = 0;
         int buy = Integer.MAX_VALUE;
         
         for (int price: prices){
             if (price < buy)
                 buy = price;
-            profit = price - buy;
-            max = Math.max(max, profit);
+            max = Math.max(max, price - buy);
         }
         return max;
     }
