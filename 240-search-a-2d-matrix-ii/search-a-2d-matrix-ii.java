9f5fb239dc6c96010019e83e8cde030aab6e43ea
Egypta Gogoi <egyptagogoi@gmail.com>
2026-05-25 23:20:15 +0530

    Time: 3 ms (88.29%) | Memory: 48.2 MB (43.64%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean BS (int [] a, int t){
+        int l = 0, r = a.length;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(a[mid] == t) return true;
+            if(a[mid] > t) r = mid - 1;
+            else l = mid + 1;
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] m, int t) {
+        for (int i = 0; i < m.length; i++){
+            if(m[i][0] <= t && t <= m[i][m[0].length-1]){
+                if (BS(m[i], t))
+                    return BS(m[i], t);
+            }
+        }
+        return false;
+    }
+}
