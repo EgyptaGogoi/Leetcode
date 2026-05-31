8520674a16ffcd942dd60b69684b70bf581d51dd
Egypta Gogoi <egyptagogoi@gmail.com>
2026-05-31 13:30:11 +0530

    Time: 27 ms (31.18%) | Memory: 107.8 MB (7.85%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i =0; i< a.length; i++){
             if (sum >= a[i]){
                 sum += a[i];
-                // System.out.println(mass);
             }
             else
                 return false;
@@ -13,22 +12,3 @@ class Solution {
         return true;
     }
 }
-
-// 4 4 9 23
-// sum = mass
-// mass > current ele    
-//     sum += 
-
-// sum = 5
-// mass > 4
-//     sum += 4 (9)
-//     sum < 4 no
-// mass > 4
-//     sum += 4(9+4 = 13)
-//     13<9 no
-// mass > 9
-//     sum += 9(13+9 =22) 
-//     22 < 23 yes
-        // return false
-
-
