1acf4bbbb5525dee7aab11c468c2c5daff09fa5c
Egypta Gogoi <egyptagogoi@gmail.com>
2026-06-02 13:47:48 +0530

    Time: 16 ms (92.52%) | Memory: 45.6 MB (47.53%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,26 +1,45 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int sum=0, diff, ret=0 ,tsum=Integer.MAX_VALUE, i, j, k;
-        if(nums.length==3)
-            return nums[0]+nums[1]+nums[2];
+        int min = Integer.MAX_VALUE;
         Arrays.sort(nums);
-        for(i=0; i<nums.length-2; i++){
-            j=i+1;
-            k=nums.length-1;
-            while(j<k){
-                sum=nums[i]+nums[j]+nums[k];
-                diff=sum-target;
-                diff=Math.abs(diff);
-                if(tsum>=diff){
-                    tsum=diff;
-                    ret=sum;
+        int clsum= nums[0]+ nums[1]+nums[2];
+        for(int i = 0; i< nums.length-2; i++){
+            int l = i+1;
+            int r = nums.length-1;
+            while(l<r){
+                int sum = nums[i]+ nums[l]+nums[r];
+                if(sum == target)
+                    return sum;
+                int dist = Math.abs(target-sum);
+                if(dist<min){   
+                    min = dist;
+                    clsum = sum;
                 }
-                if(sum > target)
-                    k--;
+                if (target> sum)
+                    l++;
                 else
-                    j++;
+                    r--;
             }
         }
-        return ret;
+        return clsum; 
     }
 }
+
+// -1 2 1 = 2
+// 2 1 -4 = -1
+// -1 1 -4 = 4
+// -1 2 -4 = -3
+
+// 2, -1, 4, 3
+
+// 1 -4 = 3
+// 1-2 = 1
+// 1-(-1) = 0
+// 1 -(-3) = 2
+
+// -4 -1 1 2
+
+// -4 -1 2 = -3
+// (1--3) -> 4 > target = r--
+// -4 -1 1 = 4
+// (1-4) -> -3 < target l++
