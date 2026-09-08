8f392f6f7eb181a5b1c009e3e49f563fde36a466
Egypta Gogoi <egyptagogoi@gmail.com>
2026-09-08 20:07:09 +0530

    Time: 15 ms (24.22%) | Memory: 46.7 MB (9.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,25 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
+        if(s.length() != t.length())
             return false;
-        HashMap<Character, Integer> hm1 = new HashMap<>();
-        HashMap<Character, Integer> hm2 = new HashMap<>();
-
-        for (char c : s.toCharArray()) {
-            hm1.put(c, hm1.getOrDefault(c, 0) + 1);
+        HashMap<Character,Integer> freq_s = new HashMap<>();
+        HashMap<Character,Integer> freq_t = new HashMap<>();
+        for(char c: s.toCharArray()){
+            freq_s.put(c, freq_s.getOrDefault(c, 0)+1);
         }
-
-        for (char c : t.toCharArray()) {
-            hm2.put(c, hm2.getOrDefault(c, 0) + 1);
+        for(char c: t.toCharArray()){
+            freq_t.put(c, freq_t.getOrDefault(c, 0)+1);
         }
-
-        for (Map.Entry<Character, Integer> entry : hm1.entrySet()) {
-            char key = entry.getKey();
-
-            if (!hm2.containsKey(key) || 
-                !hm2.get(key).equals(entry.getValue())) {
+        for(Character key : freq_s.keySet()){
+            if(!freq_s.get(key).equals(freq_t.get(key)))
                 return false;
-            }
         }
         return true;
     }
