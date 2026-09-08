f97e83e64ce72012a2696372f9575b507199ef35
Egypta Gogoi <egyptagogoi@gmail.com>
2026-09-08 20:07:50 +0530

    Time: 4 ms (83.76%) | Memory: 46.3 MB (47.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())
-            return false;
-        HashMap<Character,Integer> freq = new HashMap<>();
-        for(char c: s.toCharArray()){
-            freq.put(c, freq.getOrDefault(c, 0)+1);
-        }
-        for(char c: t.toCharArray()){
-            freq.put(c, freq.getOrDefault(c, 0)-1);
-        }
-        for(Integer f : freq.values()){
-            if(f!=0)
-                return false;
-        }
-        return true;
+        char[] arr_s = s.toCharArray();
+        char[] arr_t = t.toCharArray();
+        Arrays.sort(arr_s);
+        Arrays.sort(arr_t);
+        if(Arrays.equals(arr_s, arr_t))
+            return true;
+        return false;
     }
 }
